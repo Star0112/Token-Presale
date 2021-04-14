a7f31844d67da19ea92e84ec57dadfc12dc02673
star0112 <dreamstar30112@gmail.com>
2021-04-14 08:31:14 +0400

    add mainnet

--- src/config/index.js
+++ src/config/index.js
@@ -11,11 +11,11 @@ const config = {
   networkId: process.env.REACT_APP_NETWORK_ID,
   contractAddress: {
     fpan: {
-      56: '',
+      56: '0x141636a93652c76F4Ffc5C9f7Fd9B74F4F46e3c5',
       97: '0x341DB60C0BeAE36Ba6Bc72cE6e0c5bFe200791fc',
     },
     presale: {
-      56: '',
+      56: '0x38Cc1E72272946367AbB02435fA20F3AEaEC2872',
       97: '0xc7e3ea78717eACF10507c63E3708Aed213678B75',
     }
   }
